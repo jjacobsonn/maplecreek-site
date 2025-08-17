6ab7a23f9dade50babe5956a9f9e33fbc578cce3
Jackson Jacobson <cjacobsonnn@gmail.com>
2025-08-16 22:48:51 -0600

    Fix: Made service button bold

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -298,7 +298,7 @@ export default function AboutPage() {
               <svg className="inline-block w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
               </svg>
-              Services
+              <span className="font-bold">Services</span>
             </div>
             <h2 className="text-4xl font-extrabold mb-3 text-gray-900 font-montserrat">Our In-Home Services</h2>
             <p className="text-lg text-gray-700 font-montserrat max-w-3xl mx-auto">
