22b338b56af5ae0246f4314091953fe8dc9c9616
Jackson Jacobson <cjacobsonnn@gmail.com>
2025-10-08 13:24:17 -0600

    Added favicon icons and implemented onto site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,35 @@ const montserrat = Montserrat({
 export const metadata: Metadata = {
   title: "Maple Creek Home Health & Hospice",
   description: "Compassionate home health and hospice care services",
+  icons: {
+    icon: [
+      { url: '/images/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/images/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/images/favicon/favicon.ico', sizes: 'any' }
+    ],
+    apple: [
+      { url: '/images/favicon/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ],
+    other: [
+      {
+        rel: 'mask-icon',
+        url: '/images/favicon/favicon.ico',
+      },
+      {
+        rel: 'android-chrome-192x192',
+        url: '/images/favicon/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/images/favicon/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      }
+    ],
+  },
+  manifest: '/images/favicon/site.webmanifest',
 };
 
 export default function RootLayout({
